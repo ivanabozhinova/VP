422b43355fad1d2ae9b43919eb0d078821452cc6
Ivana <bozinovai@gmail.com>
2013-05-21 00:41:00 +0200

    MERGE

--- Game/Game/Form1.Designer.cs
+++ Game/Game/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pbTime = new System.Windows.Forms.ProgressBar();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // timer1
@@ -37,11 +39,24 @@
             this.timer1.Interval = 10;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // pbTime
+            // 
+            this.pbTime.Location = new System.Drawing.Point(2, 405);
+            this.pbTime.Maximum = 1200;
+            this.pbTime.Name = "pbTime";
+            this.pbTime.Size = new System.Drawing.Size(697, 23);
+            this.pbTime.TabIndex = 0;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(723, 321);
+            this.ClientSize = new System.Drawing.Size(741, 459);
+            this.Controls.Add(this.pbTime);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
@@ -54,6 +69,8 @@
         #endregion
 
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar pbTime;
+        private System.Windows.Forms.Timer timer2;
     }
 }
 
